f0d0036d18a5f39049eca05bde5d2cd60bf62e3c
Suzi216 <suzana.marsela@gmail.com>
2023-02-08 16:34:31 +0300

    fixing linter errors

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -10,17 +10,17 @@ describe CapitalizeDecorator do
 
       expect(capit.correct_name).to eq 'Htet'
     end
+
   end
 end
 
 describe TrimmerDecorator do
-  context 'Testing for TrimmerDecorator class' do
-    it 'Test correct_name for TrimmerDecorator class' do
-      person = Person.new(22, 'Longer_name_test')
-
-      capit = TrimmerDecorator.new(person)
-
-      expect(capit.correct_name).to eq 'Longer_nam'
+    context 'Testing for TrimmerDecorator class' do
+      it 'Test correct_name for TrimmerDecorator class' do
+        person = Person.new(22, 'Longer_name_test')
+        capit = TrimmerDecorator.new(person)
+        expect(capit.correct_name).to eq 'Longer_nam'
+      end
+  
     end
   end
-end
